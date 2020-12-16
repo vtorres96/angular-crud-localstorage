40b1bd4d40b5cf531472ec39aaec1fb32d4d8371
vtorres96 <victorcdc96@gmail.com>
2020-12-16 16:22:23 -0300

    added services: task

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  tasks: Task[];
+  public taskSelected: Task;
+  public taskForm: FormGroup;
+
+  constructor() {
+    // this.tasks = [
+    //   {title: 'Create a Website', description: 'Create a wordpress website', hide: true},
+    //   {title: 'write a document', description: 'Do other stuff', hide: true}
+    // ];
+  }
+
+  getTasks() {
+    if(localStorage.getItem('tasks') === null) {
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    return this.tasks;
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+    let tasks = [];
+    if(localStorage.getItem('tasks') === null) {
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks.push(task); 
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }
+
+  deleteTask(task: Task) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (task == this.tasks[i]) {
+        this.tasks.splice(i, 1);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      }
+    }
+  }
+}
