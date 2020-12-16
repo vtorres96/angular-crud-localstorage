86887cff9b68e60059744f4d46f77e9343089b69
vtorres96 <victorcdc96@gmail.com>
2020-12-16 16:23:30 -0300

    added app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Components
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+
+// Service
+import { TaskService } from './services/task.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    TasksListComponent,
+    TaskComponent,
+    TaskFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [TaskService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
